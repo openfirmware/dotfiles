74c1e39eaa06aaad4a1891bd08a4cdf075f55e59
James Badger <jamesbadger@gmail.com>
2012-04-30 14:47:28 -0600

    Support system-wide RVM

--- ruby/rvm.zsh
+++ ruby/rvm.zsh
@@ -2,3 +2,8 @@ if [[ -s $HOME/.rvm/scripts/rvm ]]; then
 	PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 	source $HOME/.rvm/scripts/rvm
 fi
+
+if [[ -s /etc/profile.d/rvm.sh ]]; then
+  source /etc/profile.d/rvm.sh
+fi
+
