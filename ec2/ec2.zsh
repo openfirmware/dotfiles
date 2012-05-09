4fa54e6e361207a03602777eb6874163172fd416
James Badger <jamesbadger@gmail.com>
2012-05-09 09:40:10 -0600

    Add EC2 vars

--- ec2/ec2.zsh
+++ ec2/ec2.zsh
@@ -4,4 +4,6 @@ if [[ -d "$HOME/.ec2" ]]; then
 	export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
 	export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
 	export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.2.5/jars"
+    export EC2_SSH_PRIVATE_KEY="$HOME/.ssh/id_rsa"
+    export EC2_URL="https://ec2.us-west-2.amazonaws.com"
 fi
