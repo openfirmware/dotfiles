a6247ccbecf4cc518132cebed87fc86777c1e0f1
James Badger <jamesbadger@gmail.com>
2013-02-24 23:53:02 -0700

    Add path for NPM binaries

--- bin/paths.zsh
+++ bin/paths.zsh
@@ -2,5 +2,5 @@ export PATH="$PATH:$DOTFILES/bin"
 
 # Add paths for homebrew on OS X
 if [ $OSX ]; then
-	export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+	export PATH="/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 fi
