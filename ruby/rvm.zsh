888e4ea4d2913c52d7a0648b7b7d98b85be18c67
James Badger <jamesbadger@gmail.com>
2014-04-30 12:45:32 -0600

    Reorganize env loading separately

--- ruby/rvm.zsh
+++
@@ -1,9 +0,0 @@
-if [[ -s $HOME/.rvm/scripts/rvm ]]; then
-	PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-	source $HOME/.rvm/scripts/rvm
-fi
-
-if [[ -s /etc/profile.d/rvm.sh ]]; then
-  source /etc/profile.d/rvm.sh
-fi
-
