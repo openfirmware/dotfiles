7f04ba42ff4cc9bad13d260fb77b69cb6f7df544
James Badger <jamesbadger@gmail.com>
2014-04-30 12:59:41 -0600

    Improve include for postgres.app bin

--- osx/path.zsh
+++ osx/path.zsh
@@ -1,3 +1,9 @@
 if [ $OSX ]; then
-  export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+  # If Postgres.app is installed then include its bin
+  pg_app_bin="/Applications/Postgres.app/Contents/Versions/9.3/bin"
+  if [[ -d $pg_app_bin ]]; then
+    export PATH="$pg_app_bin:$PATH"
+  fi
+
+  export PATH="/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 fi
