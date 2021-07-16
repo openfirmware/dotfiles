70141a15c3ea7aabcd2278a352bdef326797f2bc
James Badger <james@jamesbadger.ca>
2021-07-16 13:57:34 -0600

    Add git abbreviations
    Also always use verbose commits that show a preview of the diff in
    the commit editor.

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -1,6 +1,10 @@
 #!/usr/bin/env fish
 
+abbr ga 'git add'
+abbr gc 'git commit'
+abbr gca 'git commit --all'
 abbr glog "git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+abbr gp 'git push'
 abbr gs 'git status -sb'
 abbr grm "git status | grep deleted | awk '{print \$3}' | xargs git rm"
 
