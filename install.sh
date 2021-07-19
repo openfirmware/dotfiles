a4cd32f382d05acbbdbe9ac24e88153ee0182f4b
James Badger <james@jamesbadger.ca>
2021-07-19 15:12:26 -0600

    Fix duplicate vim symlink
    Issue would cause a recursive vim.symlink file to be created in
    ~/.vim on every installer run. This only occurred for the directory
    symlink, and was fixed by using the -h option to avoid creating a
    link inside the link target. Instead the link target should be
    replaced.

--- install.sh
+++ install.sh
@@ -30,14 +30,14 @@ fi
 
 # Install symlinks to configurations
 function link_dotfile() {
-	ln -Ffsv "$1" "$2"
+	ln -Ffhsv "$1" "$2"
 }
 
-linkables=$(find . -maxdepth 2 -name "*.symlink" -print)
+linkables=$(find $(pwd) -maxdepth 2 -name "*.symlink" -print)
 for file in $linkables
 do
 	filename=$(basename "$file" .symlink)
-	link_dotfile $(pwd)/$file $HOME/.$filename
+	link_dotfile "$file" "$HOME/.$filename"
 done
 
 echo "# Installing fzf #"
